f7a5c7f2541149354ba48cd20e50c52cc94b7562
alexei <silvathebest@asocial.team>
2023-05-14 17:06:33 +0400

    enable ssl

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,16 @@
-import {Sequelize} from "sequelize"
+import {Sequelize} from 'sequelize'
 import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME || 'postgres',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD,
-    {
-        dialect: "postgres",
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT)
-    }
+  process.env.DB_NAME || 'postgres',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    ssl: true
+  }
 )
