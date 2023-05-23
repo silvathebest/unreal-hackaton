f62a879a934ac3ab8475f6da0586e51ec57b4e31
BalconFront <vova.myltsin@yandex.ru>
2023-05-23 17:04:26 +0400

    filters template

--- client/src/pages/reports/index.tsx
+++ client/src/pages/reports/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {ReactSVG} from 'react-svg'
 import {PageHeader} from 'features'
 import {Sidebar} from 'widgets'
+import filtersIcon from './icon/filters.svg'
+import ordersIcon from './icon/orders.svg'
+import searchIcon from './icon/search.svg'
 import styles from './styles.module.scss'
 
 const Reports = () => {
@@ -8,9 +12,29 @@ const Reports = () => {
     <div className={styles.root}>
       <Sidebar />
 
-      <div className={styles.workArea}>
+      <div className={styles.workAreaWrapper}>
         <PageHeader>Отчёты</PageHeader>
 
+        <div className={styles.workArea}>
+          <div className={styles.filters}>
+            <div className={styles.search}>
+              <ReactSVG src={searchIcon} className={styles.icon} />
+              <input type='text' className={styles.searchInput} placeholder='Поиск' />
+            </div>
+
+            <div className={styles.button}>
+              <ReactSVG src={filtersIcon} className={styles.icon} /> Фильтры
+            </div>
+
+            <div className={styles.button}>
+              <ReactSVG src={ordersIcon} className={styles.icon} />Сортировка
+            </div>
+          </div>
+
+          <div>
+            Карточки
+          </div>
+        </div>
       </div>
     </div>
   )
